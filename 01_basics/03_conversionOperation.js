4059588c432ff3371dc161d7d1ee0288853492fc
Gaurav Singh <141561295+gaurav-singh2004@users.noreply.github.com>
2025-02-28 20:17:56 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "33abc"
+// let score = null
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 and false => 0
+
+// let isLoggedIn = 1
+// console.log(typeof isLoggedIn);
+
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanLoggedIn);
+
+// 1 => true 
+// 0 => false 
+// "" => false
+// "Gaurav" => true
+
+
+let someNumber = 24052004
+
+let someNumberInString = String(someNumber)
+console.log(someNumberInString);
+console.log(typeof someNumberInString)
