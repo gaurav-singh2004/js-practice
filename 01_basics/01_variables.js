874df8905d40a9a3467a0e0981714c41c63117b9
Gaurav Singh <141561295+gaurav-singh2004@users.noreply.github.com>
2025-02-28 19:26:53 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12217
+let accountEmail = "gaurav.singh2022@vitstudent.ac.in"
+var accountPassword = "12345"
+city = "Greater Noida"
+let accountState
+// accountId = 12    //not allowed 
+/*
+preferred not to use var keyword
+because of issue in block scope and functional scope
+*/
+accountEmail = "dpsvn.gaur12217@gmail.com"
+accountPassword = "234567"
+city = "Noida"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,city,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+// alert(3+3) //we are using nodejs not browser 
+
+// number => 2 power 53 
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value (representation of empty value)
+// undefined 
+// symbol => unique 
+
+
+
+// object 
+
+console.log(typeof "Gaurav Singh"); //output : string
+console.log(typeof 23);  // output : number
+console.log(typeof null); // output : object 
+console.log(typeof undefined); // output : undefined
+
+
